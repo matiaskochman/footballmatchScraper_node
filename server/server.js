e6f47072140d898a8a2a081f225aeb1f4e0da165
matias <matiaskochman@gmail.com>
2018-01-10 17:56:13 +0100

    finished parsing

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ var parser = require('./parser.js');
 const port = process.env.PORT || 3000;
 
 
-var jornadaBase = 70;
-var url_incomplete = 'https://www.eurosport.es/_ajax_/results_v8_5/results_teamsports_v8_5.zone?O2=1&site=ese&langueid=6&dropletid=150&domainid=141&sportid=22&revid=309&seasonid=96&mime=text%2fxml&DeviceType=desktop&roundid=51'
+var jornadaBase = 170;
+var url_incomplete = 'https://www.eurosport.es/_ajax_/results_v8_5/results_teamsports_v8_5.zone?O2=1&site=ese&langueid=6&dropletid=150&domainid=141&sportid=22&revid=309&seasonid=96&mime=text%2fxml&DeviceType=desktop&roundid=5'
 
 
 var app = express();
@@ -24,13 +24,14 @@ app.get('/data',(req,resp) => {
   var results = [];
 
   var promises = [];
+  var urls = [];
 
-
-  for(jornada=1;jornada<20;jornada++){
+  for(jornada=0;jornada<38;jornada++){
     var requestObj = {};
     var jordanaCompuesta = jornadaBase+jornada
     var url = url_incomplete.concat(jordanaCompuesta);
 
+    urls[jornada] = url;
     promises[jornada] = new Promise(function(resolve,reject){
       parser.parseHtml(url).then(function(res){
         resolve(res);
@@ -46,10 +47,16 @@ app.get('/data',(req,resp) => {
 
   Promise.all(promises).then(values => {
     let base = 'j';
-    let jornada = {};
+    let jornadas = {};
+    let count = 1;
     values.map(function(val,index){
-      let jornada_index = base.concat(index+1);
-      jornada[jornada_index] = val;
+
+      if(val && val.length > 0){
+        let jornada_index = base.concat(count);
+        jornada[jornada_index] = val;
+        count++;
+
+      }
 
       //console.log(jornada);
 
@@ -59,7 +66,7 @@ app.get('/data',(req,resp) => {
     });
 
     //console.log(results);
-    resp.status(200).send(jornada);
+    resp.status(200).send({jornadas,urls});
 
   }).catch(function(err){
     console.log(err)
