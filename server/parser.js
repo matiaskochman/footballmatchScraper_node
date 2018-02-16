be24de26f55245f1a371384136908d386c34be34
matias <matiaskochman@gmail.com>
2018-02-16 22:58:09 +0100

    finished refactoring parseResults_live 2

--- server/parser.js
+++ server/parser.js
@@ -100,94 +100,9 @@ const parseResults_live = (children,resultados) =>{
 
     children.each(function(index,tag){
 
-
-      if(tag
-        && tag.attribs
-        && tag.attribs.class
-        && tag.attribs.class.includes('date-wrapper')){
+      parseFechaDia(tag,index)
 
 
-          var obj = {};
-          var partidos = [];
-          obj.partidos = [];
-          obj.key = index;
-          var match_date_list = [];
-          var match_date_anterior = '';
-          let count_partidos = 0;
-
-          if(tag.children[0]
-            && tag.children[0].attribs
-            && tag.children[0].attribs.class
-            && tag.children[0].attribs.class.includes('date-caption')){
-
-              //var t1 = tag.children[0].children[0].children[1].children[0].data;
-              let match_date = tag.children[0].children[0].data;
-              //console.log(match_date)
-              if(match_date !== match_date_anterior){
-                obj.match_date = match_date
-                match_date_anterior = match_date;
-              }
-
-              var fecha_partido = {}
-              fecha_partido.fecha = match_date;
-              fecha_partido.partidos = [];
-              var partidos =  tag.children[1].children;
-
-              partidos.map(function(partido_tag,index){
-
-                if(partido_tag.children[0]
-                  && partido_tag.children[0].children[0]
-                  && partido_tag.children[0].children[0].children[1]
-                  && partido_tag.children[0].children[0].children[1].attribs
-                  && partido_tag.children[0].children[0].children[1].attribs.class
-                  && partido_tag.children[0].children[0].children[1].attribs.class.includes('team__name')
-                  && partido_tag.children[0].children[2]
-                  && partido_tag.children[0].children[2].children[1]
-                  && partido_tag.children[0].children[2].children[1].attribs
-                  && partido_tag.children[0].children[2].children[1].attribs.class
-                  && partido_tag.children[0].children[2].children[1].attribs.class.includes('team__name')){
-
-
-                    let team1 = partido_tag.children[0].children[0].children[1].children[0].data;
-                    let team2 = partido_tag.children[0].children[2].children[1].children[0].data;
-                    if(!partido_tag.children[0].children[1].children[1].children){
-
-                      var score1 = 'x';
-                      var score2 = 'x';
-                      var match_state = 'notStarted'
-                      var match_time = partido_tag.children[0].children[1].children[1].data;
-                    }else{
-                      var match_state = 'finished'
-                      var score1 = partido_tag.children[0].children[1].children[1].children[0].data;
-                      var score2 = partido_tag.children[0].children[1].children[3].children[0].data;
-                    }
-
-                    let partido = {team1,team2,score1,score2,match_time,match_state}
-                    fecha_partido.partidos.push(partido);
-                    //console.log(partido)
-                }
-
-
-              });
-              fecha.push(fecha_partido);
-              //console.log(fecha_partido);
-          }
-
-      }
-
-
-/*
-      if(tag && tag.attribs && tag.attribs.class && tag.attribs.class.includes('date-wrapper')){
-        tag.children.map(function(d01,i01){
-          if(d01){
-            obj.date = d01.data;
-            objDate = d01.data;
-            resultados.push(obj);
-          }
-        });
-
-      }
-*/
 
     });
 
@@ -203,121 +118,6 @@ const parseResults_live = (children,resultados) =>{
 }
 
 
-/**************************************/
-
-
-const parseResults = (children,resultados) => {
-  return new Promise((resolve,reject) => {
-    var objDate = '';
-    children.each(function(index,tag){
-
-      let obj = {};
-      obj.key = index;
-
-      if(tag && tag.attribs && tag.attribs.class && tag.attribs.class.includes('date-caption')){
-        tag.children.map(function(d01,i01){
-          if(d01){
-            obj.date = d01.data;
-            objDate = d01.data;
-            resultados.push(obj);
-          }
-        })
-      } else if(tag && tag.attribs && tag.attribs.class && tag.attribs.class === 'match'){
-
-        let state = {};
-        let match = {};
-        state.match = match;
-
-        //team1
-        if(tag.children[0]
-          && tag.children[0].children[0]
-          && tag.children[0].children[0].children[1]
-          && tag.children[0].children[0].children[1].attribs
-          && tag.children[0].children[0].children[1].attribs.class
-          && tag.children[0].children[0].children[1].attribs.class.includes('team__name')){
-
-            //var t1 = tag.children[0].children[0].children[1].children[0].data;
-            var team1 = tag.children[0].children[0].children[1].children[0].children[0].data;
-        }
-          //team2
-        if(tag
-            &&tag.children[0]
-            && tag.children[0].children[2]
-            && tag.children[0].children[2].children[1]
-            && tag.children[0].children[2].children[1].children[0]
-            && tag.children[0].children[2].children[1].children[0].children[0]
-            && tag.children[0].children[2].children[1].children[0].children[0].data
-            && tag.children[0].children[2].children[1].attribs.class.includes('team__name')){
-
-              var team2 = tag.children[0].children[2].children[1].children[0].children[0].data;
-              //var t2 = tag.children[0].children[2].children[1].children[0].data;
-        }
-        obj.localTeam = team1;
-        obj.visitorTeam = team2;
-        //obj.state = state;
-
-        //time
-        if(tag.children[0]
-          && tag.children[0].children[1]
-          && tag.children[0].children[1].attribs.class.includes('match__time')
-          && tag.children[0].children[1].children[1]
-          && tag.children[0].children[1].children[1].data){
-
-            //var score1 = tag.children[0].children[1].children[1].children[0].data;
-            var time = tag.children[0].children[1].children[1].data;
-            //obj.state.match.matchState = "notStarted";
-            //obj.state.date = objDate +' '+ time;
-            obj.matchState = "notStarted";
-            obj.date = objDate +' '+ time;
-
-        }
-
-        //score1
-        if(tag.children[0]
-          && tag.children[0].children[1]
-          && tag.children[0].children[1].attribs.class.includes('match__scores')
-          && tag.children[0].children[1].children[1]
-          && tag.children[0].children[1].children[1].children[0]
-          && tag.children[0].children[1].children[1].children[0].data){
-
-            var score1 = tag.children[0].children[1].children[1].children[0].data;
-            //obj.state.match.localResult = score1;
-            obj.localResult = score1;
-        }
-
-        //score2
-        if(tag
-          &&tag.children[0]
-          &&tag.children[0].children[1]
-          && tag.children[0].children[1].attribs.class.includes('match__scores')
-          &&tag.children[0].children[1].children[3]
-          &&tag.children[0].children[1].children[3].children[0]
-          &&tag.children[0].children[1].children[3].children[0].data){
-
-          var score2 = tag.children[0].children[1].children[3].children[0].data;
-          //obj.state.match.visitorResult = score2;
-          //obj.state.match.matchState = "finished";
-
-          obj.visitorResult = score2;
-          obj.matchState = "finished";
-        }
-/*
-        calculatePoints(obj).then(function(points){
-          obj.points = points;
-        })
-*/
-        resultados.push(obj);
-      }
-    })
-
-    if(resultados){
-      resolve(resultados);
-    }else{
-      reject(resultados);
-    }
- });
-}
-
 const calculatePoints = (obj) => {
   return new Promise((resolve,reject) => {
     if(obj
@@ -328,4 +128,85 @@ const calculatePoints = (obj) => {
     }
   })
 }
+
+const parsePartido = (partido_tag,fecha_partido) => {
+
+  if(partido_tag.children[0]
+    && partido_tag.children[0].children[0]
+    && partido_tag.children[0].children[0].children[1]
+    && partido_tag.children[0].children[0].children[1].attribs
+    && partido_tag.children[0].children[0].children[1].attribs.class
+    && partido_tag.children[0].children[0].children[1].attribs.class.includes('team__name')
+    && partido_tag.children[0].children[2]
+    && partido_tag.children[0].children[2].children[1]
+    && partido_tag.children[0].children[2].children[1].attribs
+    && partido_tag.children[0].children[2].children[1].attribs.class
+    && partido_tag.children[0].children[2].children[1].attribs.class.includes('team__name')){
+
+
+      let team1 = partido_tag.children[0].children[0].children[1].children[0].data;
+      let team2 = partido_tag.children[0].children[2].children[1].children[0].data;
+      if(!partido_tag.children[0].children[1].children[1].children){
+
+        var score1 = 'x';
+        var score2 = 'x';
+        var match_state = 'notStarted'
+        var match_time = partido_tag.children[0].children[1].children[1].data;
+      }else{
+        var match_state = 'finished'
+        var score1 = partido_tag.children[0].children[1].children[1].children[0].data;
+        var score2 = partido_tag.children[0].children[1].children[3].children[0].data;
+      }
+
+      let partido = {team1,team2,score1,score2,match_time,match_state}
+      fecha_partido.partidos.push(partido);
+      //console.log(partido)
+  }
+}
+
+const parseFechaDia = (tag,index) =>{
+
+  if(tag
+    && tag.attribs
+    && tag.attribs.class
+    && tag.attribs.class.includes('date-wrapper')){
+
+      var obj = {};
+      var partidos = [];
+      obj.partidos = [];
+      obj.key = index;
+      var match_date_list = [];
+      var match_date_anterior = '';
+      let count_partidos = 0;
+
+      if(tag.children[0]
+        && tag.children[0].attribs
+        && tag.children[0].attribs.class
+        && tag.children[0].attribs.class.includes('date-caption')){
+
+          //var t1 = tag.children[0].children[0].children[1].children[0].data;
+          let match_date = tag.children[0].children[0].data;
+          //console.log(match_date)
+          if(match_date !== match_date_anterior){
+            obj.match_date = match_date
+            match_date_anterior = match_date;
+          }
+
+          var fecha_partido = {}
+          fecha_partido.fecha = match_date;
+          fecha_partido.partidos = [];
+          var partidos =  tag.children[1].children;
+
+          partidos.map(function(partido_tag,index){
+            parsePartido(partido_tag,fecha_partido);
+          });
+
+          fecha.push(fecha_partido);
+          //console.log(fecha_partido);
+      }
+
+  }
+
+}
+
 module.exports = {parseHtml};
