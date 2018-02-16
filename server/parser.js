299513f3c75989ddeef17e455a750d76a8dfa80c
matias <matiaskochman@gmail.com>
2018-02-16 22:43:14 +0100

    finished refactoring parseResults_live

--- server/parser.js
+++ server/parser.js
@@ -7,7 +7,7 @@ const j2 = 'https://www.eurosport.es/_ajax_/results_v8_5/results_teamsports_v8_5
 
 const j19 = 'https://www.eurosport.es/_ajax_/results_v8_5/results_teamsports_v8_5.zone?O2=1&site=ese&langueid=6&dropletid=150&domainid=141&sportid=22&revid=309&seasonid=96&mime=text%2fxml&DeviceType=desktop&roundid=5189'
 
-parseHtml = function(url){
+parseHtml = function({url,live}){
   /*
   var jornadaBase = 70;
   var jordanaCompuesta = jornadaBase+day
@@ -18,6 +18,8 @@ parseHtml = function(url){
   return new Promise((resolve,reject) => {
 
       https.get(url, (res) => {
+
+        //handle result
         const { statusCode } = res;
         const contentType = res.headers['content-type'];
 
@@ -28,34 +30,56 @@ parseHtml = function(url){
         }
 
         if (error) {
+          //handle error
           console.error(error.message);
           // consume response data to free up memory
           res.resume();
           return;
         }
-
         res.setEncoding('utf8');
-
-        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('data', (chunk) => {
+          //downloading data
+          rawData += chunk;
+        });
         res.on('end', () => {
+          // finished downloading
           try {
             const $ = cheerio.load(rawData, {
                 normalizeWhitespace: true,
                 xmlMode: true
             });
             var resultados = [];
-            let children = $('div').attr('xmlns:exsl', 'http://exslt.org/common').children();
 
-            if(children){
-              parseResults(children,resultados).then((res) => {
-                resolve(res);
-              }).catch(function(err){
-                console.error('error: '+err);
-                reject(err);
-              });
+            if(!live){
+              var children = $('div').attr('xmlns:exsl', 'http://exslt.org/common').children();
+
+            }else{
+              //var node = $('div').attr('class', 'ajax-data');
+              var children = $('div').children().first().children();
 
             }
 
+            if(children){
+
+              if(!live){
+                parseResults(children,resultados).then((res) => {
+                  resolve(res);
+                }).catch(function(err){
+                  console.error('error: '+err);
+                  reject(err);
+                });
+              }else{
+                //aca parsea live results_v8_5
+
+                parseResults_live(children,resultados).then((res) => {
+                  resolve(res);
+                }).catch(function(err){
+                  console.error('error: '+err);
+                  reject(err);
+                });
+
+              }
+            }
           } catch (e) {
             console.error(e.message);
           }
@@ -64,15 +88,126 @@ parseHtml = function(url){
       }).on('error', (e) => {
         console.error(`Got error: ${e.message}`);
       });
+  });
+}
 
+const parseResults_live = (children,resultados) =>{
+  return new Promise((resolve,reject) => {
+
+    var objDate = '';
+
+    fecha = [];
+
+    children.each(function(index,tag){
+
+
+      if(tag
+        && tag.attribs
+        && tag.attribs.class
+        && tag.attribs.class.includes('date-wrapper')){
+
+
+          var obj = {};
+          var partidos = [];
+          obj.partidos = [];
+          obj.key = index;
+          var match_date_list = [];
+          var match_date_anterior = '';
+          let count_partidos = 0;
+
+          if(tag.children[0]
+            && tag.children[0].attribs
+            && tag.children[0].attribs.class
+            && tag.children[0].attribs.class.includes('date-caption')){
+
+              //var t1 = tag.children[0].children[0].children[1].children[0].data;
+              let match_date = tag.children[0].children[0].data;
+              //console.log(match_date)
+              if(match_date !== match_date_anterior){
+                obj.match_date = match_date
+                match_date_anterior = match_date;
+              }
+
+              var fecha_partido = {}
+              fecha_partido.fecha = match_date;
+              fecha_partido.partidos = [];
+              var partidos =  tag.children[1].children;
+
+              partidos.map(function(partido_tag,index){
+
+                if(partido_tag.children[0]
+                  && partido_tag.children[0].children[0]
+                  && partido_tag.children[0].children[0].children[1]
+                  && partido_tag.children[0].children[0].children[1].attribs
+                  && partido_tag.children[0].children[0].children[1].attribs.class
+                  && partido_tag.children[0].children[0].children[1].attribs.class.includes('team__name')
+                  && partido_tag.children[0].children[2]
+                  && partido_tag.children[0].children[2].children[1]
+                  && partido_tag.children[0].children[2].children[1].attribs
+                  && partido_tag.children[0].children[2].children[1].attribs.class
+                  && partido_tag.children[0].children[2].children[1].attribs.class.includes('team__name')){
+
+
+                    let team1 = partido_tag.children[0].children[0].children[1].children[0].data;
+                    let team2 = partido_tag.children[0].children[2].children[1].children[0].data;
+                    if(!partido_tag.children[0].children[1].children[1].children){
+
+                      var score1 = 'x';
+                      var score2 = 'x';
+                      var match_state = 'notStarted'
+                      var match_time = partido_tag.children[0].children[1].children[1].data;
+                    }else{
+                      var match_state = 'finished'
+                      var score1 = partido_tag.children[0].children[1].children[1].children[0].data;
+                      var score2 = partido_tag.children[0].children[1].children[3].children[0].data;
+                    }
+
+                    let partido = {team1,team2,score1,score2,match_time,match_state}
+                    fecha_partido.partidos.push(partido);
+                    //console.log(partido)
+                }
+
+
+              });
+              fecha.push(fecha_partido);
+              //console.log(fecha_partido);
+          }
+
+      }
+
+
+/*
+      if(tag && tag.attribs && tag.attribs.class && tag.attribs.class.includes('date-wrapper')){
+        tag.children.map(function(d01,i01){
+          if(d01){
+            obj.date = d01.data;
+            objDate = d01.data;
+            resultados.push(obj);
+          }
+        });
+
+      }
+*/
+
+    });
+
+    console.log(fecha);
+    resultados.push(fecha);
+    if(resultados){
+      resolve(resultados);
+    }else{
+      reject(resultados);
+    }
   });
 
 }
 
+
+/**************************************/
+
+
 const parseResults = (children,resultados) => {
-
   return new Promise((resolve,reject) => {
-
     var objDate = '';
     children.each(function(index,tag){
 
@@ -93,7 +228,6 @@ const parseResults = (children,resultados) => {
         let match = {};
         state.match = match;
 
-
         //team1
         if(tag.children[0]
           && tag.children[0].children[0]
@@ -105,7 +239,6 @@ const parseResults = (children,resultados) => {
             //var t1 = tag.children[0].children[0].children[1].children[0].data;
             var team1 = tag.children[0].children[0].children[1].children[0].children[0].data;
         }
-
           //team2
         if(tag
             &&tag.children[0]
@@ -123,14 +256,12 @@ const parseResults = (children,resultados) => {
         obj.visitorTeam = team2;
         //obj.state = state;
 
-
         //time
         if(tag.children[0]
           && tag.children[0].children[1]
           && tag.children[0].children[1].attribs.class.includes('match__time')
           && tag.children[0].children[1].children[1]
           && tag.children[0].children[1].children[1].data){
-
 
             //var score1 = tag.children[0].children[1].children[1].children[0].data;
             var time = tag.children[0].children[1].children[1].data;
@@ -140,7 +271,6 @@ const parseResults = (children,resultados) => {
             obj.date = objDate +' '+ time;
 
         }
-
 
         //score1
         if(tag.children[0]
@@ -153,9 +283,7 @@ const parseResults = (children,resultados) => {
             var score1 = tag.children[0].children[1].children[1].children[0].data;
             //obj.state.match.localResult = score1;
             obj.localResult = score1;
-
         }
-
 
         //score2
         if(tag
@@ -167,15 +295,12 @@ const parseResults = (children,resultados) => {
           &&tag.children[0].children[1].children[3].children[0].data){
 
           var score2 = tag.children[0].children[1].children[3].children[0].data;
-
           //obj.state.match.visitorResult = score2;
           //obj.state.match.matchState = "finished";
 
           obj.visitorResult = score2;
           obj.matchState = "finished";
-
         }
-
 /*
         calculatePoints(obj).then(function(points){
           obj.points = points;
--- server/server.js
+++ server/server.js
@@ -7,10 +7,13 @@ const cheerio = require('cheerio');
 var parser = require('./parser.js');
 const port = process.env.PORT || 3000;
 
+//https://www.eurosport.es/_ajax_/live_v8_5/livebox_v8_5.zone?O2=1&site=ese&langueid=6&dropletid=150&domainid=141&sportid=22&revid=309&AdPageName=home-event&mime=text%2fxml&DeviceType=desktop&roundid=5187
 
-var jornadaBase = 171;
+var jornadaBase = 171; //codigo necesario para posicionarme en la primera fecha del 2017
+
 var url_ligaA_esp_incomplete = 'https://www.eurosport.es/_ajax_/results_v8_5/results_teamsports_v8_5.zone?O2=1&site=ese&langueid=6&dropletid=150&domainid=141&sportid=22&revid=309&seasonid=96&mime=text%2fxml&DeviceType=desktop&roundid=5'
 
+var  url_ligaA_esp_incomplete_live = "https://www.eurosport.es/_ajax_/live_v8_5/livebox_v8_5.zone?O2=1&site=ese&langueid=6&dropletid=150&domainid=141&sportid=22&revid=309&AdPageName=home-event&mime=text%2fxml&DeviceType=desktop&roundid=5";
 
 var  url_ligaB_esp_incomplete = "https://www.eurosport.es/_ajax_/live_v8_5/livebox_v8_5.zone?O2=1&site=ese&langueid=6&dropletid=150&domainid=141&sportid=22&revid=313&AdPageName=home-event&mime=text%2fxml&DeviceType=desktop&roundid=5"
 
@@ -28,70 +31,90 @@ app.get('/data',(req,resp) => {
   var results = [];
 
   var promises = [];
-  var urls = [];
+  var promises_live = [];
 
+  var urls = [];
+  var urls_live = [];
+
+  //iterando cada jornada para crear una query por la jornada
   for(jornada=0;jornada<38;jornada++){
     var requestObj = {};
     var jordanaCompuesta = jornadaBase+jornada
+    var url_live = url_ligaA_esp_incomplete_live.concat(jordanaCompuesta);
     var url = url_ligaA_esp_incomplete.concat(jordanaCompuesta);
     //var url = ligab.concat(jordanaCompuesta);
 
     urls[jornada] = url;
+    //urls_live[jornada] = url_live;
+    urls_live.push(url_live);
+
     promises[jornada] = new Promise(function(resolve,reject){
-      parser.parseHtml(url).then(function(res){
+      //parsear el html y convertirlo en json
+
+      //var obj = {url:url,live:false};
+      var obj = {url:url_live,live:true};
+
+      parser.parseHtml(obj).then(function(res){
         resolve(res);
       }).catch(function(err){
         reject(err);
       });
-    })
+    });
+    /*
 
-    //requestsArray.push(requestObj);
+    promises_live[jornada] = new Promise(function(resolve,reject){
+      parser.parseHtml({url:url_live,live:true}).then(function(res){
+        resolve(res);
+      }).catch(function(err){
+        reject(err);
+      });
+
+    });
+    */
   }
 
-  console.log(promises);
 
   Promise.all(promises).then(values => {
     let base = 'J';
     let jornadas = [];
     let count = 1;
+
     values.map(function(val,index){
       jornada = {};
-
       if(val && val.length > 0){
         let jornada_index = base.concat(count);
-        //jornadas[index].jornada = jornada_index;
-        //jornadas[index].val = val;
         jornada.title = jornada_index;
         jornada.list= val;
         jornadas[index] = jornada;
         count++;
-
       }
-
-      //console.log(jornada);
-
-      //results.push(jornada);
-
-      //console.log(results);
     });
-
-    //console.log(results);
-    resp.status(200).send({jornadas,urls});
-
+    resp.status(200).send({jornadas,urls,urls_live});
   }).catch(function(err){
     console.log(err)
   });
+  /*
+  Promise.all(promises_live).then(values => {
+    let base = 'J';
+    let jornadas = [];
+    let count = 1;
 
-
-  //console.log(resultados);
-  //fullObj.jornadas = results;
-/*
-  parser.parseHtml(i).then((json)=>{
-    results.push(json);
-  }).catch((error) => {
-    console.error(error);
+    values.map(function(val,index){
+      jornada = {};
+      if(val && val.length > 0){
+        let jornada_index = base.concat(count);
+        jornada.title = jornada_index;
+        jornada.list= val;
+        jornadas[index] = jornada;
+        count++;
+      }
+    });
+    resp.status(200).send({jornadas,urls});
+  }).catch(function(err){
+    console.log(err)
   });
-*/
+  */
+
 });
 
 
